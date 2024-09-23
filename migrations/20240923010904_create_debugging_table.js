3371016d0b47555e5dc8b855e777684db41d5c3a
Lauren Hutchinson <lauren.adqueen@gmail.com>
2024-09-22 19:21:50 -0700

    Fixed some issues with using the wrong questions data

--- migrations/20240923010904_create_debugging_table.js
+++ migrations/20240923010904_create_debugging_table.js
@@ -1,12 +1,12 @@
 export function up(knex) {
     return knex.schema.createTable('debuggingCodes', (table) => {
-        table.increments('id').primary();           // Auto-incrementing primary key
-        table.string('javascript').notNullable();        // Skill string
-        table.string('typescript').notNullable();        // Skill string
-        table.string('python').notNullable();        // Skill string
-        table.string('java').notNullable();        // Skill string
-        table.string('csharp').notNullable();        // Skill string
-                 // Updates associated skills if the job id is updated
+        table.increments('id').primary();          
+        table.string('javascript').notNullable();       
+        table.string('typescript').notNullable();        
+        table.string('python').notNullable();   
+        table.string('java').notNullable();        
+        table.string('csharp').notNullable();    
+
       });
 };
   
