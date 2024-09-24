55342ed012b79f46b43cf2c507794e9d0b781405
Lauren Hutchinson <lauren.adqueen@gmail.com>
2024-09-24 15:38:12 -0700

    editing cors_origin to match domain name

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const knex = initKnex(config);
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json()); 
-app.use(cors({origin: process.env.CORS_ORIGIN}));
+app.use(cors({origin: process.env.CORS_ORIGIN || 'http://localhost:5173'}));
 
 app.get('/', (req,res)=> {
     res.send("Welcome to my capstone API")
