5d56fe20edce92fc9d920108aab0b2b39dde9e75
Lauren Hutchinson <lauren.adqueen@gmail.com>
2024-09-24 14:11:10 -0700

    Updated questions table to reflect changes made in mySQL workbench

--- migrations/20240911200236_create_questions_tables.js
+++ migrations/20240911200236_create_questions_tables.js
@@ -5,7 +5,8 @@ export function up(knex) {
         table.integer('skill_id').unsigned().notNullable() 
           .references('id').inTable('skills')     
           .onDelete('CASCADE')                     
-          .onUpdate('CASCADE');                    
+          .onUpdate('CASCADE');           
+        table.string('skill_name').notNullable();         
       });
 
 };
